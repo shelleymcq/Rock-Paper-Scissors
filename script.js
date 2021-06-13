8170fcbc2c57db084173ac7812dea9ed07fa5ce9
Shelley McHardy <shelley.mchardy@gmail.com>
2021-06-13 14:24:16 -0400

    updated JS to collect player choice

---
+++ script.js
@@ -0,0 +1,34 @@
+// collect player's choice
+const playerBtn = $('button');
+
+$(".button").click(function() {
+    let playerChoice = $(this).attr('id');
+    localStorage.setItem("choice", playerChoice)
+
+
+
+
+})
+
+// computer's choice
+
+
+
+
+// render choices to screen
+
+
+
+// logic for winning
+
+
+
+// render results to screen
+
+
+
+// play again button
+
+
+
+// store scores?
