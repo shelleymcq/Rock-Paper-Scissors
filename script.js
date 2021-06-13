a8f0be35cf354019ecfcdcc0db66bb44dfa61aea
Shelley McHardy <shelley.mchardy@gmail.com>
2021-06-13 16:27:38 -0400

    disabled buttons after selection and added play again button for refresh

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 // collect player's choice
 
-$("button").click(function() {
+$(".play-button").click(function() {
     let playerChoice = $(this).attr('id');
     localStorage.setItem("choice", playerChoice)
+    $(".play-button").attr("disabled", true)
     playGame();
 })
 
@@ -19,30 +20,33 @@ let playGame = function() {
 // render choices to screen
 
 let renderChoices = function() {
-    const player = $("#player-choice").text(localStorage.getItem("choice"))
-    const computer = $("#computer-choice").text(localStorage.getItem("computer"))
+    $("#player-choice").text(localStorage.getItem("choice"))
+    $("#computer-choice").text(localStorage.getItem("computer"))
+    $("#show").removeClass("is-hidden");
     // logic for winning
     if((localStorage.getItem("choice")) === (localStorage.getItem("computer"))) {
         $("#results").removeClass("is-hidden");
         $("#results").text("Tie!")
+        $("#play-again").removeClass("is-hidden");
     } else if (((localStorage.getItem("choice")) === "rock") && ((localStorage.getItem("computer")) === "scissors")) {
         $("#results").removeClass("is-hidden");
         $("#results").text("You Win!")
+        $("#play-again").removeClass("is-hidden");
     } else if (((localStorage.getItem("choice")) === "paper") && ((localStorage.getItem("computer")) === "rock")) {
         $("#results").removeClass("is-hidden");
         $("#results").text("You Win!")
+        $("#play-again").removeClass("is-hidden");
     } else if (((localStorage.getItem("choice")) === "scissors") && ((localStorage.getItem("computer")) === "paper")) {
         $("#results").removeClass("is-hidden");
         $("#results").text("You Win!")
+        $("#play-again").removeClass("is-hidden");
     } else {
         $("#results").removeClass("is-hidden");
         $("#results").text("Computer Wins!")
+        $("#play-again").removeClass("is-hidden");
     }
-    
 }
 
-// play again button/clear screen
-
-
-
-// store scores?
+$("#play-again").click(function() {
+    location.reload();
+})
